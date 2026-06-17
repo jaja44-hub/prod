decf624d589ca243ea2a48376af3efcc368b3408
jshukrala <jshukrala@gmail.com>
2026-06-18 01:35:18 +0300

    chore(ui): remove template Banner from Dashboard (remove legacy bottom overlay)

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -16,7 +16,6 @@ import DashboardCard10 from '../partials/dashboard/DashboardCard10';
 import DashboardCard11 from '../partials/dashboard/DashboardCard11';
 import DashboardCard12 from '../partials/dashboard/DashboardCard12';
 import DashboardCard13 from '../partials/dashboard/DashboardCard13';
-import Banner from '../partials/Banner';
 
 function Dashboard() {
 
@@ -84,8 +83,6 @@ function Dashboard() {
 
           </div>
         </main>
-
-      <Banner />
 
     </div>
   );
